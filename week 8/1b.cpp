5b126829570c7523bb5d50cb56a363f2437ff273
SAKSHI SINGH <sakshisingh41345@gmail.com>
2026-04-25 12:38:25 +0530

    Refactor queue implementation using linked list

--- week 8/1b.cpp
+++ week 8/1b.cpp
@@ -1,57 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node {
-    int data;
-    struct node *next;
-};
 
-struct node *front = NULL, *rear = NULL;
+typedef struct nodetype {
+    int INFO;
+    struct nodetype *NEXT;
+} node;
 
-void enqueue(int val) {
-    struct node *t = (struct node*)malloc(sizeof(struct node));
-    t->data = val;
-    t->next = NULL;
-    if (rear == NULL) {
-        front = rear = t;
+typedef struct queue {
+    node *front, *rear;
+} linkedqueue;
+
+int isempty(linkedqueue q) {
+    if (q.front == NULL)
+        return 1;
+    else
+        return 0;
+}
+
+linkedqueue insert(linkedqueue q, int item) {
+    node *pnew = (node *)malloc(sizeof(node));
+    pnew->INFO = item;
+    pnew->NEXT = NULL;
+
+    if (q.rear == NULL) {
+        q.front = q.rear = pnew;
     } else {
-        rear->next = t;
-        rear = t;}}
+        (q.rear)->NEXT = pnew;
+        q.rear = pnew;
+    }
+    printf("Item inserted...\n");
+    return q;
+}
 
-int dequeue() {
-    if (front == NULL) {
-        printf("Queue Empty\n");
-        return -1;}
-    struct node *t = front;
-    int val = t->data;
-    front = front->next;
-    if (front == NULL) rear = NULL;
-    free(t);
-    return val;}
+linkedqueue deleteNode(linkedqueue q) {
+    node *pdel;
+    if (isempty(q)) {
+        printf("\nQueue empty - Deletion not possible\n");
+        return q;
+    }
 
-void display() {
-    struct node *p = front;
-    if (p == NULL) {
-        printf("Empty\n");
-        return;}
-    while (p != NULL) {
-        printf("%d ", p->data);
-        p = p->next;}
-    printf("\n");}
+    pdel = q.front;
+    if (q.front == q.rear) {
+        q.front = q.rear = NULL;
+    } else {
+        q.front = (q.front)->NEXT;
+    }
+    free(pdel);
+    printf("Node deleted...\n");
+    return q;
+}
+
+void traversal(linkedqueue q) {
+    if (isempty(q)) {
+        printf("\nQueue empty\n");
+    } else {
+        node *p = q.front;
+        printf("\nContents of Queue:\nFront-> ");
+        while (p != NULL) {
+            printf("%d ", p->INFO);
+            p = p->NEXT;
+        }
+        printf("<-Rear\n");
+    }
+}
 
 int main() {
-    int choice, val;
-    while (1) {
-        printf("1.Enqueue 2.Dequeue 3.Display 4.Exit: ");
+    int choice, item;
+    linkedqueue q;
+    q.front = q.rear = NULL;
+
+    do {
+        printf("\nMENU (Queue operations):");
+        printf("\n1.Insert\n2.Delete\n3.Traverse\n4.Exit\n");
+        printf("Choice ? ");
         scanf("%d", &choice);
-        if (choice == 1) {
-            printf("Enter value: ");
-            scanf("%d", &val);
-            enqueue(val);
-        } else if (choice == 2) {
-            val = dequeue();
-            if (val != -1) printf("Dequeued: %d\n", val);
-        } else if (choice == 3) {
-            display();
-        } else {
-            exit(0);}}
-    return 0;}
+
+        switch (choice) {
+            case 1:
+                printf("Enter int value to be inserted: ");
+                scanf("%d", &item);
+                q = insert(q, item);
+                break;
+            case 2:
+                q = deleteNode(q);
+                break;
+            case 3:
+                traversal(q);
+                break;
+        }
+    } while (choice != 4);
+
+    return 0;
+}
